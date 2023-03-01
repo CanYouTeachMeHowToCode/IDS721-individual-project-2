0879f717628a93fb1442a8dd011c91830e96fcaf
CanYouTeachMeHowToCode <yilunw1030@gmail.com>
2023-03-01 15:59:18 -0500

    finished

---
+++ project2/target/debug/deps/project2-f32014996e861e76.d
@@ -0,0 +1,7 @@
+/Users/wuyilun/Desktop/IDS721-individual-project-2/project2/target/debug/deps/project2-f32014996e861e76: src/main.rs
+
+/Users/wuyilun/Desktop/IDS721-individual-project-2/project2/target/debug/deps/project2-f32014996e861e76.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
