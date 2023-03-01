5461a5e2b45990f41cd10c12c27b2471ca42c9df
CanYouTeachMeHowToCode <yilunw1030@gmail.com>
2023-03-01 15:58:21 -0500

    add logs

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,14 +1,16 @@
-/*An actix Microservice that has multiple routes:
-A. / that turns a hello world
-B. /n that returns the solution of the n-queens problem with board size n
+/* An actix Microservice that has multiple routes:
+A. / that returns a welcome page
+B. /solve/n that returns the solution of the n-queens problem with board size n
 C. /version that returns the version of the service
 */
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-//import the random fruit function from the lib.rs file
 use project2::n_queens;
 
-// create a function that returns a hello world
+extern crate log;
+use log::{debug, error, log_enabled, info, Level};
+
+// create a function that returns a welcome page
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("This is a n-queens solver microservice")
@@ -17,21 +19,29 @@ async fn hello() -> impl Responder {
 // create a function that returns the n-queens problem solution with board size n
 #[get("/solve/{n}")]
 async fn solve(n: web::Path<i32>) -> impl Responder {
-    println!("providing n-queens problem solution with board size={n}");
+    info!("providing n-queens problem solution with board size={n}");
+
     HttpResponse::Ok().body(n_queens(*n))
 }
 
 // create a function that returns the version of the service
 #[get("/version")]
 async fn version() -> impl Responder {
-    println!("Version: {}", env!("CARGO_PKG_VERSION"));
+    info!("Version: {}", env!("CARGO_PKG_VERSION"));
     HttpResponse::Ok().body(env!("CARGO_PKG_VERSION"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     //add a print message to the console that the service is running
-    println!("Running the service");
+    info!("Running the service");
     HttpServer::new(|| App::new().service(hello).service(solve).service(version))
         .bind("0.0.0.0:8080")?
         .run()
